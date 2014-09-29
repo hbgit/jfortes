8daf18e393f4b594f3d29550a5b20526d5cb7685
Larissa Bentes <lgpbentes@gmail.com>
2014-09-29 11:13:41 -0400

    Added annotations grammar and test cases path.
    Modified annot_grammar.py, added annotations_grammar e and test cases
    
    Signed-off-by: Larissa Bentes <lgpbentes@gmail.com>

---
+++ modules/annotations/annot_grammar.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+"""
+Author: Herbert O. Rocha
+Email: herberthb12@gmail.com
+Year: 2014
+Version: 1
+"""
+
+from pyparsing import *
+import sys
+import re
+
+#Utils
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#following from Python cookbook, #475186
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+has_colours = has_colours(sys.stdout)
+
+def printout(text, colour=WHITE):
+        if has_colours:
+                seq = "\x1b[1;%dm" % (30+colour) + text + "\x1b[0m"
+                sys.stdout.write(seq)
+        else:
+                sys.stdout.write(text)
+
+
+listannotdata = []
+
+#----------------------------------------------------------
+# Parse actions
+tmplistdata = []
+
+def savedata(recString):
+    conc_str_token=""
+
+    for token in recString:
+        conc_str_token += str(token)
+    tmplistdata.append(1)
+    print(conc_str_token)
+
+#----------------------------------------------------------
+
+########################################################################
+#---------------------------------------------------------------
+"""
+Grammar to JFORTES code annotation
+"""
+
+letter                      = Regex(r"([a-zA-Z_]+)")
+number                      = Regex(r"([0-9]+)")
+string                      = OneOrMore(letter | letter + number)
+identifyAnnot               = Literal("//@")
+
+annotationName              = identifyAnnot + (Keyword("getSequenceConstructor").setResultsName('annot_name') |
+                                               Keyword("getSequenceMethod").setResultsName('annot_name'))
+
+attrName                    = Literal("name") + Literal("=") + (string).setResultsName('atrrName')
+attrArgs                    = Literal("args") + Literal("=") + (string | "none").setResultsName('attrArgs')
+attrSequence                = Literal("sequence") + Literal("=") + number.setResultsName('attrSequence')
+attrSequencebyconstructor   = Keyword("sequencebyconstructor") + Literal("=") + number.setResultsName('attrSeqConstr')
+
+jGetSequenceConstructor     = annotationName + attrName + Literal(",") + attrArgs + Literal(",") + \
+                              attrSequence + Literal(";")
+
+jGetSequenceMethod          = annotationName + attrName + Literal(",") + attrArgs + Literal(",") + \
+                              attrSequence + Literal(",") + attrSequencebyconstructor + Literal(";")
+
+rule                        = jGetSequenceConstructor | jGetSequenceMethod
+grammar                     = rule
+
+#---------------------------------------------------------------
+########################################################################
+
+
+
+# -------------------------------------------------
+# Main python program
+# -------------------------------------------------
+if __name__ == "__main__":
+    # CVS file to update
+    annot_csv_file = open(sys.argv[1])
+    annot_lines_csv = annot_csv_file.readlines()
+    annot_csv_file.close()
+
+    """
+    Variables gathing from input csv file
+    number line ; class name; annotation
+    """
+    list_lines_ANNOT = []
+    list_nameclass_ANNOT = []
+    list_txt_ANNOT = []
+
+    for lineClaim in annot_lines_csv:
+
+        tmp = re.split("\?",lineClaim)
+
+        tmp[0] = tmp[0].replace(" ","")
+        tmp[1] = tmp[1].replace("^\s+","")
+        tmp[2] = tmp[2].replace("^\s+","")
+
+        list_lines_ANNOT.append(tmp[0])
+        list_nameclass_ANNOT.append(tmp[1])
+        list_txt_ANNOT.append(tmp[2])
+
+
+    # print the header csv file
+    print('annot_name;atrrName;attrArgs;attrSequence;attrSeqConstr')
+
+    for index, eachannot in enumerate(list_txt_ANNOT):
+
+        # The parse moment
+        #print(eachannot)
+        try:
+            parsed_annot = grammar.parseString( eachannot ).asDict()
+
+            # print the context csv file
+            #text = parsed_annot.values()
+            #print(';'.join(text))
+
+           # print(parsed_annot)
+
+
+            tmp_list = []
+            for key, value in parsed_annot.items():
+                #print(key, '=>', value)
+                if type(value) == ParseResults:
+                    tmp_list.append(value[0])
+                else:
+                    tmp_list.append(value)
+
+            # Check if tmp_list == 4. True
+            if len(tmp_list) == 4:
+                tmp_list.insert(len(tmp_list)+1,"none")
+
+            print(';'.join(tmp_list))
+
+            #printout(str(parsed_annot.asList()), BLUE)
+            #print("")
+        ##--------------------Improve this
+        except ParseBaseException, pe:
+            printFLag = 1
+            printout(str(pe.msg), RED)
+            print("")
+            printout(str(pe.pstr), RED)
+            print("")
+            print(" "*pe.loc+"^")
+
