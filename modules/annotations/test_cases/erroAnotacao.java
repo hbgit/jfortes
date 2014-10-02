88478ef4bca60ce68ed8ad0855d619e5a010f1e4
Larissa Bentes <lgpbentes@gmail.com>
2014-10-02 10:33:51 -0400

    Added test cases
    Added test cases running annot_extractor and annot_grammar.
    
    Signed-off-by: Larissa Bentes <lgpbentes@gmail.com>

--- modules/annotations/test_cases/erroAnotacao.java
+++ modules/annotations/test_cases/erroAnotacao.java
@@ -1,25 +1,28 @@
-public class Conta{
+public class erroAnotacao{
 	String nome;
 	double saldo;
 	int codigo;
 	
-	//@jfortes_getSequenceConstructor name = Conta, args = (none), sequence = 1;
-	Conta(){
+	//@jfortes_getSequenceConstructor name = Conta, args = (noe), sequence = 1;
+	erroAnotacao(){
 		this.setSaldo(0);
 	}
 
-	//@jfortes_getSequenceConstructor name = Bag, args = (novoNome, novoSaldo), sequence = 2;
-	Conta(String novoNome, double novoSaldo){
+	//erro
+	//@jfortes_getSequenceConstructor name = Bag, args = (novoNome, novoSaldo sequence = 2;
+	erroAnotacao(String novoNome, double novoSaldo){
 		this.setNome(novoNome);
 		this.setSaldo(novoSaldo);
 	}
 
-	//@jfortes_getSequenceMethod name = setSaldo, args = none, sequence = 2, sequencebyconstructor = 1;
+	//erro
+	//@fortes_getSequenceM name = setSaldo, args = none, sequence = 2, sequencebyconstructor = 1;
 	public String getNome(){
 		return this.nome;
 	}
 
-	//@jfortes_getSequenceMethod name = setNome, args = (novoNome), sequence = 3, sequencebyconstructor = 2;
+	//erro
+	//jfortes_getSequenceMethod nome = setNome, args (novoNome), sequence = 3 sequencebyconstructor = 2;
 	public void setNome(String novoNome){
 		this.nome = novoNome;
 	}
@@ -29,7 +32,8 @@ public class Conta{
 		return this.saldo;
 	}	
 
-	//@jfortes_getSequenceMethod name = getSaldo, args = (novoSaldo), sequence = 5, sequencebyconstructor = 4; 
+	//erro
+	//@jfortes_getSequenceMethod name = setSaldo, args = (novoSaldo), seqnce = 5, sequencebyconstructor = 4; 
 	public void setSaldo(double novoSaldo){
 		this.saldo = novoSaldo;
 	}
